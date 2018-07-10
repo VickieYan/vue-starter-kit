b95157dc52c99fe0222bef5b6c902e3f68137182
VickieYan <me@wenjieyan.com>
2018-07-10 10:16:05 +0800

    :building_construction: update architecture

---
+++ src/utils/throttle.js
@@ -0,0 +1,17 @@
+export const throttle = (func, threshold = 100) => {
+    let last 
+    let deferTimer
+    return function(...args) {
+        const now = +new Date()
+        if (last && now < last + threshold) {
+            clearTimeout(deferTimer)
+            deferTimer = setTimeout(() => {
+                last = now
+                func && func.apply(this, args)
+            }, threshold)
+        } else {
+            last = now
+            func && func.apply(this, args)
+        }
+    }
+}
